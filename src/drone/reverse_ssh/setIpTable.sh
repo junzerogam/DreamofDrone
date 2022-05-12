14ce95a6bfae96fe9156a76f84551e8a249436a7
uhmm <uh9222959@gmail.com>
2022-05-13 02:02:07 +0900

    Update setIpTable.sh

--- src/drone/reverse_ssh/setIpTable.sh
+++ src/drone/reverse_ssh/setIpTable.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-homeRouterIp="210.115.229.207"
+routerIp="210.115.229.207"  # my router ip
 interfaceName="usb0"
+
+# get dynamic mobile gateway ip address from ifconfig
 mobileGateway=$(ifconfig | grep -A2 $interfaceName | grep "inet " | awk -F' ' '{print $2}' | awk -F'.' '{print $1"."$2"."$3".1"}')
 
-setRoute=$(sudo ip route add $homeRouterIp via $mobileGateway dev $interfaceName 2>&1)
-
-#sudo ip route add 1.2.3.4 via *.*.*.* dev usb0
+#sudo ip route add 210.115.229.207 via *.*.*.* dev usb0
+setRoute=$(sudo ip route add $routerIp via $mobileGateway dev $interfaceName 2>&1)
