a21b2441a7b19c0ddd173e71eed72957badf1e13
uhmm <uh9222959@gmail.com>
2022-05-13 04:04:51 +0900

    Update reverseSSH.sh

--- src/drone/reverse_ssh/reverseSSH.sh
+++ src/drone/reverse_ssh/reverseSSH.sh
@@ -7,4 +7,5 @@ routerIp="210.115.229.207" # my router ip
 mobileIpAddress=$(ifconfig | grep -A2 $mobileInterfaceName | grep "inet " | awk -F' ' '{print $2}')
 
 # run ssh command
-ssh $mobileIpAddress -f -N -T -R 2222:localhost:22 uhyeong@$routerIp -p 5001
+# "11111111" is desktop GCS password
+sshpass -p 11111111 ssh $mobileIpAddress -f -N -T -R 2222:localhost:22 uhyeong@$routerIp -p 5001
