e68a4420dac21a3897799097e67d5ba1c99bfe30
uhmm <uh9222959@gmail.com>
2022-05-13 01:58:28 +0900

    Update reverseSSH.sh

--- src/drone/reverse_ssh/reverseSSH.sh
+++ src/drone/reverse_ssh/reverseSSH.sh
@@ -3,6 +3,8 @@
 mobileInterfaceName="usb0"
 routerIp="210.115.229.207" # my router ip
 
+# get dynamic mobile address from ifconfig
 mobileIpAddress=$(ifconfig | grep -A2 $mobileInterfaceName | grep "inet " | awk -F' ' '{print $2}')
 
+# run ssh command
 ssh $mobileIpAddress -f -N -T -R 2222:localhost:22 uhyeong@$routerIp -p 5001
