72def575d3a5af83fe6d437ba1703354848dbec6
uhmm <uh9222959@gmail.com>
2022-05-13 20:16:49 +0900

    update send n recv connect

--- src/drone/server/server.py
+++ src/drone/server/server.py
@@ -3,27 +3,34 @@ from socket import *
 port = 9000
 
 # tcp
-serverSock = socket(AF_INET,SOCK_STREAM)
+serverSock = socket(AF_INET, SOCK_STREAM)
 
 # bind
-serverSock.bind(('',port))
+serverSock.bind(('', port))
 
 # listen
 serverSock.listen(5)
-print('Waiting for connection %d port'%port)
+print('Waiting for connection %d port' % port)
 
 # accept
-connectionSock,addr = serverSock.accept()
+connectionSock, addr = serverSock.accept()
 print('Connected from ',str(addr))
 
 # recv and print
 while 1:
-    distanceData = ""
-    distanceData = connectionSock.recv(1024)
-    print("distance = %s cm" %(distanceData.decode('utf-8')))
+    nothing = "Null"
+
     weightData = ""
     weightData = connectionSock.recv(1024)
     print("weight = %s g" %(weightData.decode('utf-8')))
+    connectionSock.send(nothing.encode('utf-8'))
+
+    distanceData = ""
+    distanceData = connectionSock.recv(1024)
+    print("distance = %s cm" %(distanceData.decode('utf-8')))
+    connectionSock.send(nothing.encode('utf-8'))
+
     volumePercentage = ""
     volumePercentage = connectionSock.recv(1024)
-    print("percentage = %s %" %(volumePercentage.decode('utf-8')))
+    print("percentage = %s %%\n" %(volumePercentage.decode('utf-8')))
+    connectionSock.send(nothing.encode('utf-8'))
